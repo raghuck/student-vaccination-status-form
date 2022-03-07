c943d917c8c785f2c5052e5088f6aea8ef86cea4
raghukumarck <raghu.kumar@thecareerlabs.com>
2022-03-07 12:55:33 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from "./Form"
+//import Upload from "./components/Upload";
+
+function App() {
+return (
+<div className="App">
+<Form />
+</div>
+
+);
+}
+
+export default App;
